2c3e2b76d921326a693f9fe72ef61483cf7728ad
Juan Pedro Fisanotti <fisadev@gmail.com>
2025-05-04 17:52:23 -0300

    Revert "New attempt at solving windows port issues"
    This reverts commit 241372f7f037feef7c99b877937d3272f6101dba.

--- run.py
+++ run.py
@@ -1,46 +1,26 @@
 """
 Script meant to run the app itself.
 """
+import logging
 import signal
 import sys
 from pathlib import Path
-import multiprocessing
 
 import click
 
+from dsm import config, web, jobs, logs
 
-dsm_process = None
+
+logger = logging.getLogger(__name__)
 
 
 @click.command()
 @click.option("--config-path", type=click.Path(), default="./dsm.config",
               help="Path to the configuration file (./dsm.config if not specified)")
-def main(config_path):
-    """
-    Cli interface, which launches the server manager in a separated process.
-    This is needed because otherwise when closing the app in Windows there will be a leftover
-    zombie process with the port taken, that prevents new server runs from using it (without even
-    failing, they just don't bind the port).
-    Gosh, I hate windows.
-    """
-    global dsm_process
-
-    dsm_process = multiprocessing.Process(target=run_dcs_server_manager, args=(config_path,))
-    dsm_process.start()
-    dsm_process.join()
-
-
 def run_dcs_server_manager(config_path):
     """
     Do everything needed to get the server manager up and running.
     """
-    # these imports need to be here, in the target of the subprocess, otherwise weird things break
-    # on the subprocess (like no logging, flask not being able to run, etc).
-    import logging
-    from dsm import config, web, jobs, logs
-
-    logger = logging.getLogger(__name__)
-
     config_path = Path(config_path).absolute()
 
     config.load(config_path)
@@ -59,13 +39,14 @@ def handle_terminate(signal, frame):
     """
     Handle terminate and interrupt signals to shut down the server gracefully.
     """
-    # this is what ensures that on windows the ports are really freed
-    dsm_process.terminate()
-
+    # this is needed because otherwise in Windows there will be a leftover zombie process with the
+    # port taken, that prevents new server runs from using it (without even failing, they just
+    # don't bind the port)
+    jobs.scheduler.shutdown(wait=False)
     sys.exit(0)
 
 
 if __name__ == "__main__":
     signal.signal(signal.SIGTERM, handle_terminate)
     signal.signal(signal.SIGINT, handle_terminate)
-    main()
+    run_dcs_server_manager()
